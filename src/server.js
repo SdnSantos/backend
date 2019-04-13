10b2ad3a6a4abbd7a83093f2647ea615b96b1845
SdnSantos <sidney_ssantos@hotmail.com>
2019-04-13 19:31:30 -0300

    Adiciona script start em package.json

--- src/server.js
+++ src/server.js
@@ -54,4 +54,4 @@ app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 app.use(require("./routes"));
 
 // detar a porta
-server.listen(3333);
+server.listen(process.env.PORT || 3333);
